03aa00a1b94c1bfb9ebbaf59a76f85f07ceb121f
“shevon” <657869423@qq.com>
2023-11-28 13:46:00 +0800

    修改硬编码方式，增加全局变量存储effect

--- index.js
+++ index.js
@@ -6,28 +6,38 @@ const data = {
     text:"hello world"
 }
 
+let activeEffect
+// 副作用函数定义
+function effect(fn){
+    activeEffect = fn
+    fn()
+}
+
+
 //get set 拦截设置
 const obj =  new Proxy(data,{
     get(target,key){
-        console.log("触发读取操作",key,target)
-        bucket.add(effect)
+        if(activeEffect){
+            bucket.add(activeEffect)
+        }
         return target[key]
     },
     set(target,key,newval){
-        console.log("触发设置操作",key,target,newval)
         target[key] = newval
+        console.log(bucket)
         bucket.forEach(fn => fn())
         return true
     }
 })
 
-// 副作用函数
-function effect(){
+//使用
+//问题：会两次执行  没有建立obj明确的联系
+effect(()=>{
+    console.log("effect run ")
     document.body.innerText = obj.text
-}
+});
 
-effect();
+setTimeout(() => {
+    obj.notExit = "hell vue3"
+}, 1000);
 
-setTimeout(()=>{
-    obj.text = "hello vue3"
-},1000)
